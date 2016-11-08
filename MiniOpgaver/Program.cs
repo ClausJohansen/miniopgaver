badeee423a62dd182fbad57949d8ea2f06fe90e6
Claus Johansen <secret@email.com>
2016-11-08 11:44:32 +0100

    First commit of new VS project

---
+++ MiniOpgaver/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOpgaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opgave1();
+        }
+
+        static void Opgave1()
+        {
+
+        }
+    }
+}
